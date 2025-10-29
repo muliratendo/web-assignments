d276f9cefb8a9809761b308f51176197426030dc
Mulira Tendo Mukisa <muliratendo456@gmail.com>
2025-10-29 11:23:57 +0300

    Added basic table, Curriculum vitae, friends list, friend profiles, login form, and registration form pages, and made updates to the home page

---
+++ login-form/script.js
@@ -0,0 +1,10 @@
+document.getElementById('togglePassword').addEventListener('click', function() {
+    var passwordInput = document.getElementById('pwd');
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        this.textContent = 'Hide';
+    } else {
+        passwordInput.type = 'password';
+        this.textContent = 'Show';
+    }
+});
